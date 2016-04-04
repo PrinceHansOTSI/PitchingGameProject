419f1e854e1f07105b2a8e3cfe84775656ce2086
MacLab09 <MacLab09@MM000813.local>
2016-04-04 19:21:43 -0400

    first commit

---
+++ pitchingGameProject/GameController.swift
@@ -0,0 +1,35 @@
+//
+//  GameController.swift
+//  pitchingGameProject
+//
+//  Created by MacLab09 on 3/30/16.
+//  Copyright © 2016 MacLab09. All rights reserved.
+//
+
+import UIKit
+
+class GameController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ pitchingGameProject/gameClass.swift
@@ -0,0 +1,9 @@
+//
+//  gameClass.swift
+//  pitchingGameProject
+//
+//  Created by MacLab09 on 3/29/16.
+//  Copyright © 2016 MacLab09. All rights reserved.
+//
+
+import Foundation
