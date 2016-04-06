0183c0f4bdc9296f83570bd893b65612a1239256
Brandon Crooks <BrandonMcKC@gmail.com>
2016-04-06 03:08:04 -0400

    changes after tuesday night

--- GameController.swift
+++ GameController.swift
@@ -8,13 +8,13 @@
 
 import UIKit
 
-
-
-
 class GameController: UIViewController {
 
     let z1tapGestureRecognizer = UITapGestureRecognizer()
     @IBOutlet weak var zone1Label: UILabel!
+    
+   // var inningLog: Array = [halfInning]()
+    var inningLog: [halfInning] = []
     
     var balls: Int = 0
     var strikes: Int = 0
@@ -23,23 +23,32 @@ class GameController: UIViewController {
     @IBOutlet weak var ballsTallyLabel: UILabel!
     @IBOutlet weak var strikesTallyLabel: UILabel!
     @IBOutlet weak var outsTallyLabel: UILabel!
+    @IBOutlet weak var presultLabel: UILabel!
+    
+    @IBAction func nextBatterButton(sender: AnyObject) {
+       
+        
+        thisHalfInning.increaseOuts()
+        outsTallyLabel.text = String(thisHalfInning.outs)
+        //thisHalfInning.getNextBatter()
+    }
+    var thisHalfInning = halfInning()
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
         var valueFromFirstVC: Int!
+        print(thisHalfInning.strikes)
+       
+        
+      
 //    
 //        z1tapGestureRecognizer.addTarget(self, action: "tapFunction")
 //        zone1Label.addGestureRecognizer(z1tapGestureRecognizer)
 //        zone1Label.userInteractionEnabled = true
         
-    
-        ballsTallyLabel.text = String(balls)
-        strikesTallyLabel.text = String(strikes)
-
-    
-}
+    }
 
 
 //    func tapFunction()
@@ -48,7 +57,7 @@ class GameController: UIViewController {
 //        
 //        //make the label BIGGER
 //    }
-//   
+   
    
     
     var startTouchPoint: CGPoint!
@@ -56,12 +65,12 @@ class GameController: UIViewController {
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
         super.touchesBegan(touches, withEvent: event)
         startTouchPoint = (touches.first! as UITouch).locationInView(view)
-        print("touchpoint x: ",startTouchPoint!.x,"touchpoint y: ",startTouchPoint!.y)
-        print(zone1Label.frame.contains(startTouchPoint)) //thank you
+        //print("touchpoint x: ",startTouchPoint!.x,"touchpoint y: ",startTouchPoint!.y)
+        //print(zone1Label.frame.contains(startTouchPoint)) //thank you
 
     
     }
-    /************************************************************* END TOUCH POINT IS ONLY NEEDED LATER, FOR CALCULATING PITCH MOVEMENT. DO IT LATER ********************************/
+    /************************************************************* END TOUCH POINT FUNC IS ACTIALLY SUEPR IMPORTANT ********************************/
     var endTouchPoint: CGPoint!
     
     override func touchesEnded(touches: Set<UITouch>, withEvent event: UIEvent?) {
@@ -69,29 +78,109 @@ class GameController: UIViewController {
         let endTouchPoint = (touches.first! as UITouch).locationInView(view)
         //print("end touch point: ", endTouchPoint.x, endTouchPoint.y)
         
-        if startTouchPoint != nil
+        strikesTallyLabel.text = String(thisHalfInning.strikes)
+        ballsTallyLabel.text = String(thisHalfInning.balls)
+        outsTallyLabel.text = String(thisHalfInning.outs)
+        
+        var Swing: Bool = false
+        var Contact: Bool = false
+        var Fair: Bool = false
+        
+        
+        //so, is it a strike?
+        if zone1Label.frame.contains(endTouchPoint) == true
         {
-            
-            
-            if zone1Label.frame.contains(startTouchPoint) == true
+                   }
+        //Definining a strike code copy and pasted from previous version of this same app Ive been building for months
+        //ignoring pitch m ovement still, (and inaccuracy) we will assume balls are moving fast and straight
+        //therefore, zone 1 = zone 2, meaning that where a ball is intended to be thrown is where it is actually thrown in the strike zone.
+        
+        if thisHalfInning.strikes < 2
+        {
+            //code will need to be modified to acocunt for situations where the ball is actually hit in play, which is a huge part of this sport, its how people score usually. resetting the ball and strike counts, adding a base runner,
+            if zone1Label.frame.contains(endTouchPoint) == true
             {
-                print(strikes)
-                strikes = strikes++
-                print(strikes)
+                print("Strrrrike!");presultLabel.text = "Strike!!!"
+                thisHalfInning.increaseStrikes()
+                strikesTallyLabel.text = String(thisHalfInning.strikes)
+            }
+                
+            else if Swing && !Contact
+            {
+                print("Swinging Strrrrike!"); presultLabel.text = "Strike! (Swinging)"
+                thisHalfInning.increaseStrikes()
+                
+                strikesTallyLabel.text = String(thisHalfInning.strikes)
+            }
+            else if Contact && !Fair
+            {
+                thisHalfInning.increaseStrikes()
+                print("fouled off. strike++")
+                presultLabel.text = "fouled off. Strike."
+                strikesTallyLabel.text = String(thisHalfInning.strikes)
+            }
+                //else if Contact is TRUE, and Fair is TRUE, then you got yourself a base hit. congrats, way to fuck that up. now minimize the damage and end the inning. reset balls and strikes, bring up the next batter()
+            else
+            {
+                thisHalfInning.increaseBalls()
+                presultLabel.text = "ball."
+                ballsTallyLabel.text = String(thisHalfInning.balls)
+            }
+        }
+        else if thisHalfInning.strikes == 2 // same as <2 strikes, but foul balls dont add a strike. 
+        // added functions to tell half inning class to add an out if there is a K or something
+        {
+            if zone1Label.frame.contains(endTouchPoint) == true
+            {
+                print("Strrrrike out!");presultLabel.text = "Strikeout!!! (>|) thats a backwards K"
+                //
+                thisHalfInning.resetBallStrikeCount()
+                thisHalfInning.increaseOuts()
+               
+            }
+            else if Swing && !Contact
+            {
+                print("Swinging Strrrrike!"); presultLabel.text = "Strikeout! (K)"
+                thisHalfInning.resetBallStrikeCount()
+                //thisHalfInning.increaseOuts()
+                //instead of calling the increase ouyts function 
+                nextBatterButton.text = "Continue"
+            }
+            else if Swing && Contact && !Fair
+            {
+                thisHalfInning.increaseStrikes()
+                print("foul. Go again.")
+                presultLabel.text = "Still 2 strikes. We will do that again."
             }
             else
             {
-                balls = balls++
+                thisHalfInning.increaseBalls()
+                presultLabel.text = "ball."
+                ballsTallyLabel.text = String(thisHalfInning.balls)
             }
             
         }
-        ballsTallyLabel.text = String(balls)
-        strikesTallyLabel.text = String(strikes)
+            //thisHalfInning.endTheInningFunc()
+        else if outs == 3
+        {
+            outs == 3
+            strikes == 0
+            balls == 0
+            inningLog.append(thisHalfInning)
+            thisHalfInning = halfInning()
+        }
 
-        print(zone1Label.frame.contains(endTouchPoint)) //thank you
+        
+        
+    // control balls strikes outs tally from here
+        
+    
+    
+    
+    
+    
     }
-    /********************
-     *****************************************/
+    /*************************************************************/
  
 //    let strikeZoneRect = CGRect(x: 100, y: 100, width: 150, height: 400)
 //    func CGRectContainsPoint(strikeZoneRect:CGRect, startTouchPoint: CGPoint) -> Bool{
@@ -120,6 +209,8 @@ class GameController: UIViewController {
         {
             return false
         }
+        
+        
       
     }
     
--- halfInning.swift
+++ halfInning.swift
@@ -11,9 +11,61 @@ import Foundation
 
 class halfInning{
     
-    var balls = 0
-    var strikes = 0
-    var outs = 0
+    var strikes: Int
+    var balls: Int
+    var outs: Int
+    
+    init()
+    {
+        strikes = 0
+        balls = 0
+        outs = 0
+        
+    }
+    
+    func increaseStrikes()
+    {
+        strikes = strikes + 1
+    }
+    func increaseBalls()
+    {
+        balls = balls + 1
+    }
+    
+    
+    
+    func increaseOuts()
+    {
+        outs = outs + 1
+    }
+    
+    func resetBallStrikeCount()
+    {
+        strikes = 0
+        balls = 0
+    }
+    
+    
+    func atBatResult()
+    {
+        //what animation will happen when a hit or homerun or strikeout occurs
+    }
+    
+    func getNextBatterUp()
+    {
+        //conclude the results of the last at bat and summon the next batter in the lineup 
+        //if there are still outs to be outted (less than 3 still)
+    }
+    
+    deinit{
+    }
+    
+    func endTheInningFunc()
+    {
+        
+    }
+    
+}
     
     // reset balls, strikes, outs to 0
     // reverse which team is batting, which is pitching
@@ -22,7 +74,7 @@ class halfInning{
     //touches began func to start the pitch delivery
          
     
-    }
+    
     
     
     //touches ended func to complete the pitch delivery (Bulk of code?)
